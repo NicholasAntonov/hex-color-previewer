e554b7c0aff24aad6d827872cab3cf5b795bf8f3
nicholas antonov <nicholasantonov@gmail.com>
2016-05-20 00:37:54 -0400

    Clean up code

--- hex.js
+++ hex.js
@@ -3,15 +3,17 @@ const hexRegex = /(#[0-9a-f]{6})/ig;
 // Get all text nodes that have hex codes
 const textNodes = [];
 {
-    const walk = document.createTreeWalker(document.body,NodeFilter.SHOW_TEXT,null,false);
+    const walk = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, null, false);
     let n;
-    while(n = walk.nextNode()) {
+    while((n = walk.nextNode())) {
         if (hexRegex.test(n.nodeValue)) {
             textNodes.push(n);
         }
     }
 }
 
+// Wrap the hex code in a span
 const nodes = textNodes.map(node => node.parentNode);
-
-nodes.forEach(node => node.innerHTML = node.innerHTML.replace(hexRegex, '<span class="hex-color">$&</span>'));
+nodes.forEach(node => {
+    node.innerHTML = node.innerHTML.replace(hexRegex, '<span class="hex-color">$&</span>');
+});
